0b947794e929d74c90429a09bffe1ee602248e8d
SleepSoul <spider.coopplay@gmail.com>
2023-07-24 19:36:23 +0100

    feat(ui): Add PrintUtil and version number display

--- RunControl/RunControl.lua
+++ RunControl/RunControl.lua
@@ -8,13 +8,35 @@
 ModUtil.Mod.Register("RunControl")
 
 local config = {
-    modpackVersion = "0.1.0"
+    ModpackVersion = "0.1.0"
 }
 RunControl.config = config
 
 RunControl.CurrentRunData = {}
 
+function RunControl.DisplayVersion()
+    local text_config_table = DeepCopyTable(UIData.CurrentRunDepth.TextFormat)
+    local versionString = "RunControl "..RunControl.config.ModpackVersion
+
+    PrintUtil.createOverlayLine(
+        "ModpackVersion",
+        versionString,
+        MergeTables(
+            text_config_table,
+            {
+                x_pos = 1908,
+                y_pos = 27,
+                color = Color.DimGray
+            }
+        )
+    )
+end
+
 ModUtil.LoadOnce( function()
     RunControl.CurrentRunData = RunControl.Runs.SleepSoul607Talos
     BoonControl.CurrentRunData = RunControl.CurrentRunData.Boons or {}
 end )
+
+OnAnyLoad{ function()
+    RunControl.DisplayVersion()
+end }
