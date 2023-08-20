7435bd6be1e047d83f716ac21169b8cdac83601e
SleepSoul <spider.coopplay@gmail.com>
2023-08-20 21:00:59 +0100

    fix(RoomControl): Make Flipped work consistently
    The Context.Wrap just didn't work here. Override is required.

--- RoomControl/RoomControl.lua
+++ RoomControl/RoomControl.lua
@@ -15,15 +15,6 @@ RoomControl.config = config
 
 RoomControl.CurrentRunData = {}
 
-ModUtil.Path.Context.Wrap( "LeaveRoom", function( baseFunc, ... )
-    local roomData = RCLib.GetFromList( RoomControl.CurrentRunData, { dataType = "roomFeatures", chamberNum = GetRunDepth( CurrentRun ) + 1 } )
-
-    ModUtil.Path.Wrap( "RunShopGeneration", function( baseFunc, ... )
-        CurrentRun.CurrentRoom.Flipped = roomData.Flipped or CurrentRun.CurrentRoom.Flipped
-        baseFunc( ... )
-    end, RoomControl )
-end, RoomControl )
-
 ModUtil.Path.Wrap( "DoUnlockRoomExits", function( baseFunc, run, room )
     local forcedDoors = RCLib.GetFromList( RoomControl.CurrentRunData, { dataType = "exitDoors" } ) or {}
 
@@ -151,4 +142,128 @@ ModUtil.Path.Wrap( "HandleBreakableSwap", function( baseFunc, currentRoom )
 			end
 		end
     end
+end, RoomControl )
+
+ModUtil.Path.Override( "LeaveRoom", function( currentRun, door ) -- This overrides the code that normally determines whether a room is flipped or not
+
+	local nextRoom = door.Room
+
+	ZeroSuperMeter()
+	ClearEffect({ Id = currentRun.Hero.ObjectId, All = true, BlockAll = true, })
+	StopCurrentStatusAnimation( currentRun.Hero )
+	currentRun.Hero.BlockStatusAnimations = true
+	AddTimerBlock( currentRun, "LeaveRoom" )
+	SetPlayerInvulnerable( "LeaveRoom" )
+
+	local ammoIds = GetIdsByType({ Name = "AmmoPack" })
+	SetObstacleProperty({ Property = "Magnetism", Value = 3000, DestinationIds = ammoIds })
+	SetObstacleProperty({ Property = "MagnetismSpeedMax", Value = currentRun.Hero.LeaveRoomAmmoMangetismSpeed, DestinationIds = ammoIds })
+	StopAnimation({ DestinationIds = ammoIds, Name = "AmmoReturnTimer" })
+
+	RunUnthreadedEvents( currentRun.CurrentRoom.LeaveUnthreadedEvents, currentRun.CurrentRoom )
+
+	if IsRecordRunDepth( currentRun ) then
+		thread( PlayVoiceLines, GlobalVoiceLines.RecordRunDepthVoiceLines )
+	end
+
+	ResetObjectives()
+
+	if currentRun.CurrentRoom.ChallengeEncounter ~= nil and currentRun.CurrentRoom.ChallengeEncounter.InProgress then
+		currentRun.CurrentRoom.ChallengeEncounter.EndedEarly = true
+		currentRun.CurrentRoom.ChallengeEncounter.InProgress = false
+		thread( PlayVoiceLines, HeroVoiceLines.FleeingEncounterVoiceLines, false )
+	end
+
+	if currentRun.CurrentRoom.CloseDoorsOnUse then
+		CloseDoorForRun( currentRun, door )
+	end
+
+	RemoveRallyHealth()
+	if not nextRoom.BlockDoorHealFromPrevious then
+		CheckDoorHealTrait( currentRun )
+	end
+	local removedTraits = {}
+	for _, trait in pairs( currentRun.Hero.Traits ) do
+		if trait.RemainingUses ~= nil and trait.UsesAsRooms ~= nil and trait.UsesAsRooms then
+			UseTraitData( currentRun.Hero, trait )
+			if trait.RemainingUses ~= nil and trait.RemainingUses <= 0 then
+				table.insert( removedTraits, trait )
+			end
+		end
+	end
+	for _, trait in pairs( removedTraits ) do
+		RemoveTraitData( currentRun.Hero, trait )
+	end
+
+	local exitFunctionName = currentRun.CurrentRoom.ExitFunctionName or door.ExitFunctionName or "LeaveRoomPresentation"
+	local exitFunction = _G[exitFunctionName]
+	exitFunction( currentRun, door )
+
+	TeardownRoomArt( currentRun, currentRoom )
+	if not currentRun.Hero.IsDead then
+		--On Zag death cleanup is already processed
+		CleanupEnemies()
+	end
+	killTaggedThreads( RoomThreadName )
+	killWaitUntilThreads( "RequiredKillEnemyKilledOrSpawned" )
+	killWaitUntilThreads( "AllRequiredKillEnemiesDead" ) -- Can exist for a TimeChallenge encounter
+	killWaitUntilThreads( "RequiredEnemyKilled" ) -- Can exist for a TimeChallenge encounter
+
+	RemoveTimerBlock( currentRun, "LeaveRoom" )
+	if currentRun.CurrentRoom.TimerBlock ~= nil then
+		RemoveTimerBlock( currentRun, currentRun.CurrentRoom.TimerBlock )
+	end
+	SetPlayerVulnerable( "LeaveRoom" )
+
+	if currentRun.CurrentRoom.SkipLoadNextMap then
+		return
+	end
+
+	MoneyObjects = {}
+	OfferedExitDoors = {}
+
+    -- CHANGES MADE HERE
+    local forcedRoomData = RCLib.GetFromList( RoomControl.CurrentRunData, { dataType = "roomFeatures", chamberNum = GetRunDepth( CurrentRun ) + 1 } )
+    local printFlipped = tostring( forcedRoomData.Flipped ) or "NIL"
+    DebugPrint({ Text = "Should be flipped? " .. printFlipped })
+    if forcedRoomData.Flipped ~= nil then
+        nextRoom.Flipped = forcedRoomData.Flipped
+    else
+        local flipMap = false
+        if currentRun.CurrentRoom.ExitDirection ~= nil and nextRoom.EntranceDirection ~= nil and nextRoom.EntranceDirection ~= "LeftRight" then
+            flipMap = nextRoom.EntranceDirection ~= currentRun.CurrentRoom.ExitDirection
+        else
+            flipMap = RandomChance( nextRoom.FlipHorizontalChance or 0.5 )
+        end
+        nextRoom.Flipped = flipMap
+    end
+    -- END CHANGES
+
+	if nextRoom.Encounter == nil then
+		nextRoom.Encounter = ChooseEncounter( CurrentRun, nextRoom )
+		RecordEncounter( CurrentRun, nextRoom.Encounter )
+	end
+
+	currentRun.CurrentRoom.EndingHealth = currentRun.Hero.Health
+	currentRun.CurrentRoom.EndingAmmo = GetWeaponProperty({ Id = currentRun.Hero.ObjectId, WeaponName = "RangedWeapon", Property = "Ammo" })
+	table.insert( currentRun.RoomHistory, currentRun.CurrentRoom )
+	UpdateRunHistoryCache( currentRun, currentRun.CurrentRoom )
+	local previousRoom = currentRun.CurrentRoom
+	currentRun.CurrentRoom = nextRoom
+
+	RunShopGeneration( currentRun.CurrentRoom )
+
+	GameState.LocationName = nextRoom.LocationText
+	RandomSetNextInitSeed()
+	if not nextRoom.SkipSave then
+		SaveCheckpoint({ StartNextMap = nextRoom.Name, SaveName = "_Temp", DevSaveName = CreateDevSaveName( currentRun ) })
+		ValidateCheckpoint({ Value = true })
+
+	end
+
+	RemoveInputBlock({ Name = "MoveHeroToRoomPosition" })
+	AddInputBlock({ Name = "MapLoad" })
+
+	LoadMap({ Name = nextRoom.Name, ResetBinks = previousRoom.ResetBinksOnExit or currentRun.CurrentRoom.ResetBinksOnEnter, LoadBackgroundColor = currentRun.CurrentRoom.LoadBackgroundColor })
+
 end, RoomControl )
