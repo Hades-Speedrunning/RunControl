da183fb912c683e8f85a25e44ac1db9d341d7508
SleepSoul <spider.coopplay@gmail.com>
2023-08-21 07:43:48 +0100

    feat(RewardControl): Add support for startingReward data type
    exitDoors rewards are based on door index and wrapped on door unlock, so a separate data type is used for c1 rewards.

--- RewardControl/RewardControl.lua
+++ RewardControl/RewardControl.lua
@@ -14,7 +14,28 @@ RewardControl.config = config
 
 RewardControl.CurrentRunData = {}
 
-ModUtil.Path.Context.Wrap( "DoUnlockRoomExits", function( baseFunc, run, room )
+ModUtil.Path.Wrap( "ChooseRoomReward", function( baseFunc, run, room, rewardStoreName, previouslyChosenRewards, args ) -- c1 reward
+    local rewardToUse = baseFunc( run, room, rewardStoreName, previouslyChosenRewards, args )
+    local forcedReward = RCLib.GetFromList( RewardControl.CurrentRunData, { dataType = "startingReward" } )
+
+    if not ( RewardControl.config.Enabled ) then
+		return rewardToUse
+	end
+    if room.Name ~= "RoomOpening" then -- Other rewards are handled separately
+		return rewardToUse
+	end
+    
+    if forcedReward.Reward then
+        rewardToUse = RCLib.EncodeRoomReward( forcedReward.Reward ) or rewardToUse
+    end
+    if forcedReward.Name then
+        room.ForceLootName = RCLib.EncodeBoonSet( forcedReward.Name )
+    end
+
+    return rewardToUse
+end, RewardControl)
+
+ModUtil.Path.Context.Wrap( "DoUnlockRoomExits", function( baseFunc, run, room ) -- All other rewards
     local forcedDoors = RCLib.GetFromList( RewardControl.CurrentRunData, { dataType = "exitDoors" } )
 
     ModUtil.Path.Wrap( "ChooseRoomReward", function( baseFunc, ... )
@@ -22,7 +43,7 @@ ModUtil.Path.Context.Wrap( "DoUnlockRoomExits", function( baseFunc, run, room )
         local doorReward
         local baseReward = baseFunc( ... )
 
-        if forcedDoors[doorIndex] and forcedDoors[doorIndex].Reward then
+        if RewardControl.config.Enabled and forcedDoors[doorIndex] and forcedDoors[doorIndex].Reward then
             doorReward = RCLib.EncodeRoomReward( forcedDoors[doorIndex].Reward )
         end
 
@@ -31,9 +52,10 @@ ModUtil.Path.Context.Wrap( "DoUnlockRoomExits", function( baseFunc, run, room )
 
     ModUtil.Path.Wrap( "SetupRoomReward", function( baseFunc, currentRun, room, previouslyChosenRewards, args )
         local doorIndex = ModUtil.Locals.Stacked().index
+        local forcedGodName = ModUtil.IndexArray.Get( forcedDoors, { doorIndex, "GodName" } )
 
-        if room.ChosenRewardType == "Boon" and not room.ForceLootName and forcedDoors[doorIndex] and forcedDoors[doorIndex].GodName then
-            room.ForceLootName = RCLib.EncodeBoonSet( forcedDoors[doorIndex].GodName )
+        if RewardControl.config.Enabled and room.ChosenRewardType == "Boon" and not room.ForceLootName and forcedGodName then
+            room.ForceLootName = RCLib.EncodeBoonSet( forcedGodName )
         end
         
         baseFunc( currentRun, room, previouslyChosenRewards, args )
